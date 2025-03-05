f4f26abaaf1865c28d9cefe6fbf6a99e470c59db
RoseAnnIrish <roseannirishdelacruz@gmail.com>
2025-03-06 00:46:50 +1300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from "./components/HelloWorld";
+import Second from "./components/Second";
+import {useState} from "react";
+import ListPosts from "./components/ListPosts";
+import ListNotes from "./components/ListNotes";
+import CreateNote from "./components/CreateNote";
+import UpdateNote from "./components/UpdateNote";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListNotes/>
+        <CreateNote/>
     </div>
   );
 }
